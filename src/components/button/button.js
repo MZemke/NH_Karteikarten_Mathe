2820852c63bf8ad554920092d3d118c8ec922b0f
M.Zemke <zemke@tesolva.de>
2022-11-09 13:27:41 +0100

    first scructuring

---
+++ src/components/button/button.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components/macro';
+
+export const Button = styled.button`
+    width: 200px;
+    height: 30px;
+    background-color: yellow;
+`;
+
+export const SettingsButton = styled(Button)`
+    margin: 10px 10px 10px 10px;
+`;
